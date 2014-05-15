11d3acd1f86666f5ddba654f0611b13415c87f42
yilinx <yilinx@gmail.com>
2014-05-15 14:40:58 +0800

    completed best.R

--- best.R
+++ best.R
@@ -1,5 +1,6 @@
 best <- function(state, outcome) {
-        OutcomeRead <- read.csv("outcome-of-care-measures.csv")   ## Read outcome data
+        OutcomeRead <- read.csv("outcome-of-care-measures.csv", colClasses="character")   ## Read outcome data; important: set colClasses to character, 
+                                                                                          ##else the various mortality rate columns will be read as factor, and will screw up the order function
         
         StateList <- as.character(names(table(OutcomeRead$State)))      ##Lists all the states present
                                                                         ##table function creates the freq table of States
@@ -7,8 +8,8 @@ best <- function(state, outcome) {
                                                                         ##convert the list of state names to character
         ## Check that state and outcome are valid
         
-        TestStates <- grep(state,StateList)   
-        TestOutcome <- grep(outcome,c("heart attack","heart failure","pneumonia"))
+        TestStates <- grep(state,StateList)   ## Check States
+        TestOutcome <- grep(outcome,c("heart attack","heart failure","pneumonia")) ##Check outcome
         
         if (length(TestStates) == 0){
                 stop("invalid state")   
@@ -21,5 +22,17 @@ best <- function(state, outcome) {
         ## Return hospital name in that state with lowest 30-day death
         ## rate
         
-        
+        NormOutcome <- gsub(" ", "_",outcome) #normalize the "outcome" input by replacing " " with "_" 
+        ReduceOR <- OutcomeRead[,c(2,7,11,17,23)] #select columns of interest
+        names(ReduceOR) <- c("Hospital","State","heart_attack","heart_failure","pneumonia") ##rename columns
+        ReduceOR <- ReduceOR[ReduceOR$State==state,c("Hospital","State",NormOutcome)] ##select State of interest
+        ReduceOR <- ReduceOR[ReduceOR[,3]!="Not Available",]  ##remove entries with "Not Available"
+        ReduceOR2 <- data.frame(as.character(ReduceOR[,1]),as.numeric(ReduceOR[,3]))  #construct new dataframe with the right classes
+        names(ReduceOR2) <- c("Hospital",NormOutcome)   #rename ReduceOR2
+        OrderHospital <- order(ReduceOR2[,2],ReduceOR2[,1], decreasing=F)  #find new order using column 2 while column 1 as the tie-breaker
+        CompareHospital <- ReduceOR2[OrderHospital,]   ##re-order ReduceOR2
+
+        head(CompareHospital, n=1) ##best Hospital
+
+      
 }
